cc99b14c861420b644ff4ff3bf1dd58b9a9cec18
Jay <jmlgreer@gmail.com>
2024-05-12 20:33:53 +1000

    Formatting and basic data analysis

--- solve_bus_number_game.py
+++ solve_bus_number_game.py
@@ -7,12 +7,16 @@ DES:
 """
 
 # Import
+import time
+from matplotlib import pyplot as plt
+
 from src.world_state import WorldState
 from src.problem import Problem
 from src.tree_search import tree_search
 from src.actions import add, sub, mult, div
 from src.strategies import uniform_cost_strategy
-import time
+
+
 
 def solve_bus_number_game(numbers:tuple) -> tuple[bool, int]:
     available_actions = [add, sub, mult, div]
@@ -26,6 +30,8 @@ def main():
     total_seen = set()
     start_time = time.time()
     n_errored = 0
+    moves = []
+    unsuccessful_moves = []
     for i in range(10000):  # Adjust range if you need more or fewer cases
         # Format number as a 4-digit string with leading zeros
         number_str = f"{i:04d}"
@@ -35,22 +41,32 @@ def main():
         # Skip processing if this tuple has already been processed
         if digits in total_seen:
             continue
-        else:
-            total_seen.add(digits)
+
+        total_seen.add(digits)
 
         try:
             success, n_moves = solve_bus_number_game(digits)
             if success:
                 #print(f"Winner! {digits} solved in {n_moves} moves")
                 solved.add(digits)
+                moves.append(n_moves)
+            else:
+                unsuccessful_moves.append(n_moves)
         except Exception as e:
             print(f"Error processing {digits}: {e}")
             n_errored += 1
             continue  # Skip to the next iteration
 
     # Print the number of unique digit sets found so far
-    print(f"\n\nWe solved {len(solved)} of {len(total_seen)} {len(solved) / len(total_seen):.2f} problems in {time.time() - start_time:.2f} seconds")
+    print(f"\n\nWe solved {len(solved)} of {len(total_seen)} ({len(solved) / len(total_seen):.2f}) problems in {time.time() - start_time:.2f} seconds")
+    print(f"Average number of moves: {sum(moves) / len(solved):.2f}")
     print(f"There were {n_errored} troublesome cases")
+
+    print(sum(unsuccessful_moves))
+    print(len(unsuccessful_moves))
+
+    #plt.hist(unsuccessful_moves)
+    #plt.show()
 
 if __name__ == "__main__":
     main()
