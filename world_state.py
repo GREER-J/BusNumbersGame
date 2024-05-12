e04d737117a267fd4c85c9293cff83fbc5a4bed8
Jay <jmlgreer@gmail.com>
2024-05-12 17:11:25 +1000

    Created basic state. Not tested.:

--- world_state.py
+++ world_state.py
@@ -1,32 +1,39 @@
 from dataclasses import dataclass
-from enum import Enum, auto
-
-class Locations(Enum):
-    KEY_LOC = auto()
-    CAR_LOC = auto()
-    SHOPS_LOC = auto()
-    TA_LOC = auto()
-
-class Objects(Enum):
-    Car = auto()
+from typing import Union
 
 @dataclass
 class WorldState:
-    has_keys: bool
-    in_car: bool
-    location: Locations
+    runnning_total: float
+    n1: Union[int, bool]
+    n2: Union[int, bool]
+    n3: Union[int, bool]
+    n4: Union[int, bool]
 
     @property
     def valid_state(self) -> bool:
-        if(self.in_car and self.has_keys == False):
-            return(False)
-        else:
-            return(True)
+        return True
     
     @property
-    def score(self) -> int:
-        return(1)
+    def score(self) -> float:
+        target = 10
+        if self.runnning_total:
+            # We're up and running
+            rv = (target - self.runnning_total)**2
+            if not self.n1:
+                # N1 is there
+                rv += 1
+            if not self.n2:
+                # N1 is there
+                rv += 1
+            if not self.n3:
+                # N1 is there
+                rv += 1
+            if not self.n4:
+                # N1 is there
+                rv += 1
+            return rv
+
     
     def __repr__(self) -> str:
-        rv = f"(l: {self.location.name}, k: {self.has_keys}, c: {self.in_car})"
+        rv = f"{self.runnning_total} ->({self.n1},{self.n2}, {self.n3}, {self.n4}) left"
         return rv
